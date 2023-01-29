e44eaed4e4991d304b990aa3f30cf0aaae66bf82
GilazovDEV <60614125+GilazovDEV@users.noreply.github.com>
2023-01-29 20:56:17 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,33 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  let oH1 = document.querySelector('#countdown');
+let nSec = 10;
+
+function fCountdown() {
+  oH1.textContent = (nSec < 10 ? '0' + nSec : nSec);
+  if (!nSec) {
+    clearInterval(tCountdown);
+    setTimeout(() => {
+      nSec = 10;
+      tCountdown = setInterval(fCountdown, 1000);
+    }, 5000);
+  }
+  nSec--;
+}
+
+let tCountdown = setInterval(fCountdown, 1000);
